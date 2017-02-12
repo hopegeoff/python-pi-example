27463846944eb1b95272c8f05e40f0911f8928da
Geoff <geoff.hope1@ntlworld.com>
2017-02-12 13:13:10 +0000

    comment amended

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def main(msg):
-    // Add a different comment
-    // Print a message
+    # Add a different comment
+    # Print a message
     print(msg)
 
 main("Hello people")
